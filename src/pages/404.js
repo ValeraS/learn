7aa870cabe32c9d43747615063eb0593e74d53ef
Valeriy <ValeraS@users.noreply.github.com>
2018-08-13 01:37:45 +0300

    fix: random quotes for 404 page flickers

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 
@@ -8,21 +8,41 @@ import { quotes } from '../../static/json/quotes.json';
 
 let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
+class RandomQuote extends PureComponent {
+  state = {
+    show: false
+  }
+
+  componentDidMount() {
+    /* eslint-disable react/no-did-mount-set-state */
+    this.setState({ show: true });
+  }
+
+  render() {
+    return this.state.show ? (
+      <Fragment>
+        <p>We couldn't find what you were looking for, but here is a quote:</p>
+
+        <div className='quote-wrapper'>
+          <p className='quote'>
+            <span>&#8220;</span>
+            {randomQuote.quote}
+          </p>
+          <p className='author'>- {randomQuote.author}</p>
+        </div>
+      </Fragment>
+    ) : null;
+  }
+}
+
 const NotFoundPage = () => (
   <div className='notfound-page-wrapper'>
     <Helmet title='Page Not Found | freeCodeCamp' />
 
     <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
     <h1>NOT FOUND</h1>
-    <p>We couldn't find what you were looking for, but here is a quote:</p>
 
-    <div className='quote-wrapper'>
-      <p className='quote'>
-        <span>&#8220;</span>
-        {randomQuote.quote}
-      </p>
-      <p className='author'>- {randomQuote.author}</p>
-    </div>
+    <RandomQuote />
 
     <Link className='btn-curriculum' to='/'>
       View the Curriculum
